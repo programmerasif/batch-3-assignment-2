3ec51ae4192d542a4b4671cfbb425cc9636be3c9
programmerasif <mdasifkhan3757@gmail.com>
2024-05-23 01:30:49 +0600

    dedline misseed due to load shedding. all worked was finished in time but i did not abal to push my code into github. if possible then please consider for 60 marks its an 1 and 27 minutes hour late

--- src/app/modules/product/product.controlar.ts
+++ src/app/modules/product/product.controlar.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from 'express';
 import { productService } from './product.services';
-import {
-  ProductValidationSchema,
-} from './product.validation';
+import { ProductValidationSchema } from './product.validation';
 import { Error } from 'mongoose';
 // creat produce
 const creatProduct = async (req: Request, res: Response) => {
@@ -38,11 +36,18 @@ const getAllProduct = async (req: Request, res: Response) => {
       const result = await productService.getAllProductIntoDB(
         searchTerm as string,
       );
-      res.status(200).json({
-        success: true,
-        message: 'Products fetched successfully!',
-        data: result,
-      });
+      if (result.length == 0) {
+        res.status(500).json({
+          success: false,
+          message: 'Products not found',
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: 'Products fetched successfully!',
+          data: result,
+        });
+      }
     } else {
       const result = await productService.getAllProductIntoDB();
       res.status(200).json({
@@ -64,48 +69,68 @@ const getAllProduct = async (req: Request, res: Response) => {
 };
 // find my Specific id
 const getSpecificProduct = async (req: Request, res: Response) => {
-  const id = req.params.productId;
-  const result = await productService.getSpecificProductIntoDB(id);
-  res.status(200).json({
-    success: true,
-    message: 'Products fetched successfully!',
-    data: result,
-  });
+  try {
+    const id = req.params.productId;
+    const result = await productService.getSpecificProductIntoDB(id);
+    res.status(200).json({
+      success: true,
+      message: 'Products fetched successfully!',
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: true,
+      message: 'Products not found!',
+    });
+  }
 };
 // update by specfic id
 const updateSpecificProduct = async (req: Request, res: Response) => {
-  const id = req.params.productId;
-  const newProduct = req.body;
-  const zodPrsedData = ProductValidationSchema.parse(newProduct);
-  const result = await productService.updateSpecificProductIntoDB(
-    id,
-    zodPrsedData,
-  );
-  res.status(200).json({
-    success: true,
-    message: 'Product updated successfully!',
-    data: result,
-  });
+  try {
+    const id = req.params.productId;
+    const newProduct = req.body;
+    const zodPrsedData = ProductValidationSchema.parse(newProduct);
+    const result = await productService.updateSpecificProductIntoDB(
+      id,
+      zodPrsedData,
+    );
+    res.status(200).json({
+      success: true,
+      message: 'Product updated successfully!',
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Product not found!',
+    });
+  }
 };
 // delete document
 const deleteSpecificProduct = async (req: Request, res: Response) => {
   const id = req.params.productId;
-  const result = await productService.deleteSpecificProductIntoDB(id);
+  try {
+    const result = await productService.deleteSpecificProductIntoDB(id);
 
-  if (result) {
-    res.status(200).json({
-      success: true,
-      message: 'Product deleted successfully!',
-      data: null,
-    });
-  } else {
+    if (result) {
+      res.status(200).json({
+        success: true,
+        message: 'Product deleted successfully!',
+        data: null,
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: 'Product not found',
+      });
+    }
+  } catch (error) {
     res.status(404).json({
       success: false,
       message: 'Product not found',
     });
   }
 };
-
 
 export const productControlars = {
   creatProduct,
